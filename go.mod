11250e4342bb5abb4467e85355a5bd2e0e8c66d2
dani3l0 <daniel.jank@protonmail.com>
2023-12-07 13:42:34 +0100

    Disk SMART bringup

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module statee
 
 go 1.21.2
+
+require github.com/anatol/smart.go v0.0.0-20230705044831-c3b27137baa3
+
+require golang.org/x/sys v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"statee/syslib/disks"
-	"time"
 )
 
 func main() {
-	for {
-		hmm, _ := disks.GetDisk("sda")
-		fmt.Println(hmm)
-		time.Sleep(time.Second)
-	}
+	disks.GetSmartData("/dev/sda")
 }
