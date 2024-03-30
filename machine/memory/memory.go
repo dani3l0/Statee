eba8c135856dda3c2394b53e59593cd79583c2f1
dani3l0 <daniel.jank@protonmail.com>
2024-03-30 22:22:05 +0100

    machine: Add cache size to memory info

--- machine/memory/memory.go
+++ machine/memory/memory.go
@@ -5,6 +5,7 @@ import utils "statee/machine/utils"
 type Memory struct {
 	Used          int
 	Available     int
+	Cached        int
 	Total         int
 	SwapUsed      int
 	SwapAvailable int
@@ -16,12 +17,16 @@ func GetMemory() Memory {
 	var memory Memory
 	meminfo, _ := utils.Cat("/proc/meminfo")
 
-	mem_total, _ := utils.GrepInt(meminfo, "MemTotal")
+	// RAM
 	mem_available, _ := utils.GrepInt(meminfo, "MemAvailable")
-	memory.Total = mem_total / 1000
 	memory.Available = mem_available / 1000
+	mem_cached, _ := utils.GrepInt(meminfo, "Cached")
+	memory.Cached = mem_cached / 1000
+	mem_total, _ := utils.GrepInt(meminfo, "MemTotal")
+	memory.Total = mem_total / 1000
 	memory.Used = memory.Total - memory.Available
 
+	// Swap or zram
 	swap_total, _ := utils.GrepInt(meminfo, "SwapTotal")
 	swap_free, _ := utils.GrepInt(meminfo, "SwapFree")
 	memory.SwapTotal = swap_total / 1000
