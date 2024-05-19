e2af89ecee13dccc63aee381d21bec9e07843a31
dani3l0 <daniel.jank@protonmail.com>
2024-05-19 22:00:26 +0200

    web: Bringup dependencies

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,4 +1,23 @@
+<!-- Imports -->
+<script>
+	import "@fontsource/quicksand/600.css"
+	import * as Icons from 'lucide-svelte'
+</script>
+
+
+<!-- Main wrapper -->
 <main>
+	<Icons.ArrowDown />
 	<h1>Statee</h1>
 	<p>Yeah, it works</p>
 </main>
+
+
+<!-- Global, system-wide styling -->
+<style>
+	:global(body) {
+		background: #000;
+		color: #DDD;
+		font-family: 'Quicksand', sans-serif;
+	}
+</style>
